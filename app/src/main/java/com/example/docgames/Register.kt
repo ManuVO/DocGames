114b4ae57144b35d4cbc50428fdbc09e69794c24
Manuel <manuelvillegasobregon@gmail.com>
2022-10-23 14:18:22 +0200

    Se ha cambiado el login

--- app/src/main/java/com/example/docgames/Register.kt
+++ app/src/main/java/com/example/docgames/Register.kt
@@ -1,11 +1,28 @@
 package com.example.docgames
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val tvLogin : TextView = findViewById(R.id.tvIrLogin)
+        tvLogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/docgames/Usuario.kt
@@ -0,0 +1,3 @@
+package com.example.docgames
+
+data class Usuario(val id: Int = -1, val nombre: String, val email: String, val pass: String)
