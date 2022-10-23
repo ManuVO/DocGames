b82c771860ba08bda67871ccdc3859a24ab570d2
AriaHG <cherrerias@alu.ucam.edu>
2022-10-23 21:50:26 +0200

    he cambiado el mensaje

--- app/src/main/java/com/example/docgames/Home.kt
+++ app/src/main/java/com/example/docgames/Home.kt
@@ -3,6 +3,9 @@ package com.example.docgames
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.PopupMenu
+import android.widget.Toast
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,26 @@ class Home : AppCompatActivity() {
         val intent : Intent = intent
         val email = intent.getStringExtra("email")
         println(email)
+    }
+    public fun showMenu(v: View){
+        val menuPopup = PopupMenu(this,v)
+        menuPopup.inflate(R.menu.menu_home)  // importo el menu : menu_home.xml
+        menuPopup.setOnMenuItemClickListener {  // establece funcionalidad a los botones del menu
+            when(it.itemId){
+                R.id.id_menu_perfil -> {
+                    val intentPerfil = Intent(this,Perfil::class.java)
+                    startActivity(intentPerfil)
+                    true
+                }
+                R.id.id_menu_juegos -> {
+                    Toast.makeText(this, "Menu Popup", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.id_menu_ajustes -> {
+                    Toast.makeText(this, "Menu Popup", Toast.LENGTH_SHORT).show()
+                    true
+                }else -> true
+            }
+        }
     }
 }
