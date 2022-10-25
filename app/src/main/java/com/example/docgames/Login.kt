c6a3a1085cfef0c33f5d69734188a0f6c9701bf8
Manuel <manuelvillegasobregon@gmail.com>
2022-10-25 10:14:53 +0200

    Se ha cambiado el login

--- app/src/main/java/com/example/docgames/Login.kt
+++ app/src/main/java/com/example/docgames/Login.kt
@@ -17,10 +17,6 @@ class Login : AppCompatActivity() {
 
         // Inicializamos la variable de la BBDD
         val dataBaseHelper = DataBaseHelper(applicationContext)
-        // Obtienemos la BBDD en modo lectura
-        val db_reader = dataBaseHelper.readableDatabase
-        // Obtienemos la BBDD en modo escritura
-        val db_writer = dataBaseHelper.writableDatabase
 
 
         //BOTON DE INICIO DE SESIÓN
@@ -28,14 +24,20 @@ class Login : AppCompatActivity() {
         btnIniciarSesion.setOnClickListener {
             val editTextEmail : EditText = findViewById(R.id.ptEmail)
             val editTextPass : EditText = findViewById(R.id.ptPass)
-            if(editTextEmail.text.toString().equals("Manuel") && editTextPass.text.toString().equals("1234")) {
+            //USUARIO por defecto: admin CONTRASEÑA por defecto: admin
+            if(editTextEmail.text.toString().equals("admin") && editTextPass.text.toString().equals("admin")) {
                 val intent = Intent(this, Home::class.java)
                 intent.putExtra("email", editTextEmail.text.toString())
                 intent.putExtra("pass", editTextEmail.text.toString())
                 startActivity(intent)
             }
             else{
-                Toast.makeText(this, "No se han introducido los cambios correctamente", Toast.LENGTH_LONG).show()
+                //ENTRA A HOME SIEMPRE (CAMBIAR MAS ADELANTE)
+                val intent = Intent(this, Home::class.java)
+                intent.putExtra("email", editTextEmail.text.toString())
+                intent.putExtra("pass", editTextEmail.text.toString())
+                startActivity(intent)
+                //Toast.makeText(this, "No se han introducido los cambios correctamente", Toast.LENGTH_LONG).show()
             }
         }
 
