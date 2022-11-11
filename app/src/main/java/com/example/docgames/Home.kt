2a23b8cdbbcf769d7f05730ae902d6de506f11ac
victor9889 <victormusik98@hotmail.com>
2022-11-11 14:11:28 +0100

    Cambios Generales

--- app/src/main/java/com/example/docgames/Home.kt
+++ app/src/main/java/com/example/docgames/Home.kt
@@ -3,36 +3,120 @@ package com.example.docgames
 import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.PopupMenu
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.VideoView
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import java.security.AccessController.getContext
 
 class Home : AppCompatActivity() {
+
+    // creating variables for listview
+    lateinit var programmingLanguagesLV: ListView
+
+    // creating array adapter for listview
+    lateinit var listAdapter: ArrayAdapter<String>
+
+    // creating array list for listview
+    lateinit var programmingLanguagesList: ArrayList<String>;
+
+    // creating variable for searchview
+    lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         //sonido boton pacman
-        val mediaPlayer : MediaPlayer = MediaPlayer.create(this, R.raw.pacman)
-
-        val intent : Intent = intent
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.pacman)
+        var cerrado: Boolean = true
+        val intent: Intent = intent
         val email = intent.getStringExtra("email")
         println(email)
 
         //video de novedades
-        val videoView : VideoView = findViewById(R.id.videoView)
+        val videoView: VideoView = findViewById(R.id.videoView)
         videoView.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.novedadpokemon))
         videoView.start()
 
-        val logoMenu : TextView = findViewById(R.id.logo_menu)
+        val logoMenu: TextView = findViewById(R.id.logo_menu)
         logoMenu.setOnClickListener {
             showMenu(logoMenu)
         }
+        val relativeLayout: RelativeLayout = findViewById(R.id.cl_buscarjuego)
+        // initializing variables of list view with their ids.
+        programmingLanguagesLV = findViewById(R.id.idListaJuegos)
+        searchView = findViewById(R.id.id_buscarjuegos)
+        searchView.setOnQueryTextFocusChangeListener(object : View.OnFocusChangeListener{
+            override fun onFocusChange(p0: View?, p1: Boolean) {
+                if (cerrado) {
+                    val scale: Float = applicationContext.resources.displayMetrics.density;
+                    val pixels = (150 * scale + 0.5f).toInt()
+                    relativeLayout.layoutParams.height = pixels
+                    cerrado = false
+                } else {
+                    val scale: Float = applicationContext.resources.displayMetrics.density;
+                    val pixels = (40 * scale + 0.5f).toInt()
+                    relativeLayout.layoutParams.height = pixels
+                    cerrado = true
+                }
+            }
+        })
+
+        // initializing list and adding data to list
+        programmingLanguagesList = ArrayList()
+        programmingLanguagesList.add("C")
+        programmingLanguagesList.add("C#")
+        programmingLanguagesList.add("Java")
+        programmingLanguagesList.add("Javascript")
+        programmingLanguagesList.add("Python")
+        programmingLanguagesList.add("Dart")
+        programmingLanguagesList.add("Kotlin")
+        programmingLanguagesList.add("Typescript")
+
+        // initializing list adapter and setting layout
+        // for each list view item and adding array list to it.
+        listAdapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            programmingLanguagesList
+        )
+
+        // on below line setting list
+        // adapter to our list view.
+        programmingLanguagesLV.adapter = listAdapter
+
+        // on below line we are adding on query
+        // listener for our search view.
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // on below line we are checking
+                // if query exist or not.
+                if (programmingLanguagesList.contains(query)) {
+                    // if query exist within list we
+                    // are filtering our list adapter.
+                    listAdapter.filter.filter(query)
+                } else {
+                    // if query is not present we are displaying
+                    // a toast message as no  data found..
+                    Toast.makeText(this@Home, "No Language found..", Toast.LENGTH_LONG)
+                        .show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // if query text is change in that case we
+                // are filtering our adapter with
+                // new text on below line.
+                listAdapter.filter.filter(newText)
+                return false
+            }
+        })
+
+
+
+
 
     }
     public fun showMenu(v: View){
